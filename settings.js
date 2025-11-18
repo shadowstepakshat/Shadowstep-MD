8f8f50f7c9662990e5b99a9b084f30f6dec5ee90
shadowstepakshat <50055255+shadowstepakshat@users.noreply.github.com>
2025-11-19 01:20:07 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Shadowstep Bot',
+  author: '‎',
+  botName: "Shadowstep Bot",
+  botOwner: 'Shadowstep_Akshat', // Your name
+  ownerNumber: '913813567175', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.4",
+  updateZipUrl: "https://https://github.com/shadowstepakshat/Shadowstep-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
